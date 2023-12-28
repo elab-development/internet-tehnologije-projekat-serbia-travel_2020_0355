7948f7efc823bedec6b100bb14badedb2867471f
Filip Tasic <ftasic1202@gmail.com>
2023-12-28 21:41:45 +0100

    Add necessary routes in api.php

--- serbia-travel/routes/web.php
+++ serbia-travel/routes/web.php
@@ -17,7 +17,3 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::get('', [UserController::class,'']);
