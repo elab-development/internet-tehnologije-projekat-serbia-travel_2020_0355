80b590498d5a175b59e83d8c304d6ad9427b08ca
Filip Tasic <ftasic1202@gmail.com>
2024-01-05 02:46:12 +0100

    Added authentication and Started implementing authorization

--- serbia-travel/routes/api.php
+++ serbia-travel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -13,3 +14,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::resource('destinations', DestinationController::class);
 Route::resource('users', UserController::class);
 Route::resource('hotels', HotelController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+    Route::resource('destinations', DestinationController::class)->only(['index','store']); // dodaj exception handling kad neko pokusa da uradi bez autorizacije
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
