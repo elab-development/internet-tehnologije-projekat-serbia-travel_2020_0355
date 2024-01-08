5e03bb6bfa82a917da21c4bd759c549aeb2e5157
cjufi <ftasic1202@gmail.com>
2024-01-08 21:10:27 +0100

    Associate Bookings with Rooms

--- serbia-travel/app/Models/Room.php
+++ serbia-travel/app/Models/Room.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
@@ -17,5 +18,10 @@ class Room extends Model
 
     public function destination() {
         return $this->belongsTo(Hotel::class, 'hotel_id');
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
     }
 }
