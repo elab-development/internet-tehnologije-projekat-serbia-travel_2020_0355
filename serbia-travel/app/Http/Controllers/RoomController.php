b4deb2683f94f1bd96df8844560499a4acd8ac2b
ognjensea9 <ognjensea9@gmail.com>
2024-01-08 20:19:33 +0100

    Implement RoomController and RoomResource

---
+++ serbia-travel/app/Http/Controllers/RoomController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RoomResource;
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    public function index()
+    {
+        $rooms = Room::all();
+        return RoomResource::collection($rooms);
+    }
+
+    public function show(Room $room) {
+        return new RoomResource($room);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'room_number' => 'required|numeric|between:100,999',
+            'number_of_beds' => 'required|numeric|between:1,7',
+            'hotel_id' => 'required|exists:hotels,id',
+        ]);
+
+        $room = new Room();
+        $room->room_number = $validatedData['room_number'];
+        $room->number_of_beds = $validatedData['number_of_beds'];
+        $room->hotel_id = $validatedData['hotel_id'];
+
+        $room->save();
+
+        return new RoomResource($room);
+    }
+
+    public function update(Request $request, Room $room)
+    {
+        $validatedData = $request->validate([
+            'room_number' => 'required|numeric|between:100,999',
+            'number_of_beds' => 'required|numeric|between:1,7',
+            'hotel_id' => 'required|exists:hotels,id',
+        ]);
+
+        $room->room_number = $validatedData['room_number'];
+        $room->number_of_beds = $validatedData['number_of_beds'];
+        $room->hotel_id = $validatedData['hotel_id'];
+
+        $room->save();
+
+        return new RoomResource($room);
+    }
+
+    public function destroy(Room $room)
+    {
+        $room->delete();
+        return response()->json(['message' => 'Room deleted successfully']);
+    }
+}
