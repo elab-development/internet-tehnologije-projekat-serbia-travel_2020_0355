92b5ade88f481a48d6584ec63d0ab1b435ed9fd2
ognjensea9 <ognjensea9@gmail.com>
2024-01-18 22:49:04 +0100

    Added navigation bar

---
+++ serbia-travel-react/src/navBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div className="navBar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Log in</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
