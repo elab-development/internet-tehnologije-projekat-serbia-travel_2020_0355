bc36bce029639ec5208bf31d3d1570afeb5cc27c
cjufi <ftasic1202@gmail.com>
2024-01-30 21:03:06 +0100

    Start implementing Home component

---
+++ serbia-travel-react/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components';
+import logo from "../assets/logo.png";
+
+export default function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+const Nav = styled.nav`
+`;
