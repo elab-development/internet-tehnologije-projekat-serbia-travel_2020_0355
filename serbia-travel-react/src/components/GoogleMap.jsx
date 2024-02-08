d0bf7ba4318bc52f644d7b5bede03e2f3aa1cf4f
ognjensea9 <ognjensea9@gmail.com>
2024-02-08 17:59:13 +0100

    Add layers to the maps

--- serbia-travel-react/src/components/GoogleMap.jsx
+++ serbia-travel-react/src/components/GoogleMap.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Button from './Button';
 
 const GoogleMap = ({ apiKey }) => {
+    const [mapType, setMapType] = useState('roadmap');
+
     const defaultProps = {
         center: {
             lat: 44.620862,
@@ -10,14 +13,36 @@ const GoogleMap = ({ apiKey }) => {
         zoom: 15
     };
 
+    const handleMapTypeChange = (newType) => {
+        setMapType(newType);
+    };
+
     return (
         <div style={{ height: '400px', width: '100%' }}>
+            <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+                <Button text="Map" onClick={() => handleMapTypeChange('roadmap')} />
+                <span style={{ marginLeft: '10px' }} />
+                <Button text="Satelite" onClick={() => handleMapTypeChange('satellite')} />
+            </div>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: apiKey }}
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
-            />
-
+                options={{
+                    mapTypeId: mapType
+                }}
+            >
+                <div
+                    lat={44.620862}
+                    lng={21.187754}
+                    style={{
+                        width: '20px',
+                        height: '20px',
+                        backgroundColor: 'red',
+                        borderRadius: '50%'
+                    }}
+                ></div>
+            </GoogleMapReact>
         </div>
     );
 };
